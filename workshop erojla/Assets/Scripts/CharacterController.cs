d6a4e499ced353538fded5b64ce7df54f62124b9
ibar09 <iheb30abdelkader@gmail.com>
2022-11-10 17:21:36 +0100

    seance 3

--- workshop erojla/Assets/Scripts/CharacterController.cs
+++ workshop erojla/Assets/Scripts/CharacterController.cs
@@ -7,6 +7,10 @@ public class CharacterController : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     public Animator animator;
+    public float jump_power;
+    public LayerMask groundLayer;
+    public Transform groundCheker;
+    public float radius = 0.005f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,15 +22,29 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
         animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        animator.SetBool("isJumping", !groundCheck());
+        animator.SetFloat("yVelocity", rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && groundCheck())
+        {
+            rb.velocity = Vector2.up * jump_power;
+        }
     }
     private void FixedUpdate()
     {
-        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-        rb.velocity = direction * speed * Time.deltaTime;
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, rb.velocity.y);
+        rb.velocity = direction;
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
             transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1);
         }
 
     }
+
+    public bool groundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheker.position, radius, groundLayer);
+    }
+
+
 }
